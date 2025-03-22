2680fa2803afb39f43bab483858cf2255b562565
YOLOO-O <785214433@qq.com>
2025-03-22 22:20:50 +0800

    Added MultiplyThreeNums.py program

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,7 @@
+# MultiplyThreeNums.py
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+result = num1 * num2 * num3
+print("The product of the three numbers is: ", result)
